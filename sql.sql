2c254dc4ca2f2f2697cdc5d6dd0ba1c2e67f4d91
hayder <k.h.azawy@gmail.com>
2020-10-17 14:38:31 +0300

    some change

--- sql.sql
+++ sql.sql
@@ -26,7 +26,7 @@ CREATE TABLE `office_names` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-
+ 
 
 CREATE TABLE `rooms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
