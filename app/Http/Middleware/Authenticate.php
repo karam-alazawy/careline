85c848c6fdb44517c5d816792cc76157a0cef95e
hayder <k.h.azawy@gmail.com>
2021-05-26 11:36:34 +0300

    last

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('customerLogin');
+            return route('customerui.login');
         }
     }
 }
